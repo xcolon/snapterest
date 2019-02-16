00af84fc17d18c0903d80c918101218421d33c3b
Charoen Chaweechan <charoen.ch@kbtg.tech>
2019-02-16 20:08:47 +0700

    first commit web app

---
+++ source/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const reactElementnt = React.createElement('h1')
+
+ReactDOM.render(
+    reactElementnt,
+    document.getElementById('react-application')
+)
